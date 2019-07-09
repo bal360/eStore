2a879abd5b7dc5fc8333ed4c94cc4014e9736c5d
Devin Arnold <dbarnold321@gmail.com>
2019-07-09 16:23:07 -0600

    added routes and views to Users & Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,34 @@
 class ItemsController < ApplicationController
-  def index
+  before_action :find, only [:show, :edit, :update, :delete]
+    def index
+      @items = Item.all
   end
 
-  def new
+  def create
+      new
+      @item.create(allowed_params)
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def new
+      @item = Item.new
+  end
+
+private
+
+  def allowed_params
+      params.require(:items).params(:name, :description, :price, :quantity, :image)
+  end
+
+  def find
+      @item = Item.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+before_action :find, only [:show, :edit, :update, :delete]
+    def index
+        @user = User.all
+    end
+
+    def show
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(allowed_params)
+        if @user.save
+            redirect_to user_path(@user)
+        else 
+            render :new
+        end
+    end
+
+    def edit
+    end
+    
+    def update
+        if @user.update(allowed_params)
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
+    end
+
+private
+    def find
+        @user = User.find(params[:id])
+    end
+    def allowed_params
+        params.require(:item).params(:first_name, :last_name, :email, :phone_number, :payment_info)
+    end
+
 end
