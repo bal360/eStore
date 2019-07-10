d012afb96b6cefe968b4951269970de9092019ae
Blake A. Long <blake.a.long@gmail.com>
2019-07-10 15:59:30 -0600

    session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :get_user
+    def get_user
+        if (session[:user_id])
+            @user = User.find(session[:user_id])
+        end
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,18 @@ class SessionsController < ApplicationController
 
    def create 
         @user = User.find_by(username: params[:user][:username])
-        byebug
+        if (@user.authenticate(params[:user][:password]))
+            session[:user_id] = @user.id
+            redirect_to items_path
+        else 
+            @error = "Incorrect Login Credentials"
+            render "login/login"
+        end
+   end
+
+   def destroy
+        session[:user_id] = nil
         redirect_to items_path
    end
 
-
-    
 end
